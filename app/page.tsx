69ffd94a1b566800173e9deeb8884ace5d1ee7fc
SriSankeerthanaGorrela <sankeerthanagorrela@gmail.com>
2025-09-10 17:07:38 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 
     <section className="text-foreground min-h-screen flex items-center justify-center">
       <div className="text-center" >
-        <h1 className="font-heading text-6xl mb-4 text-accent">Vijay Photography</h1>
+        <h1 className="font-heading text-6xl mb-4 text-red-600 text-accent">Vijay Photography</h1>
         <p className="text-lg max-w-xl mx-auto">Capturing timeless moments with elegance and style.</p>
         <button className="mt-6 px-6 py-3 bg-accent text-background rounded-xl hover:opacity-90 transition">
           View Portfolio
