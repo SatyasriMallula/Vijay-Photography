415674f446c9778c24a4c879ad6fc41365de183f
syamkumarnallamilli <syamkumarnallamilli0@gmail.com>
2025-09-15 12:12:36 +0530

    back to top

--- app/clientLayout.tsx
+++ app/clientLayout.tsx
@@ -1,9 +1,48 @@
+// "use client";
+
+// import { usePathname } from "next/navigation";
+// import Header from "./components/Header";
+// import SocialLinks from "./components/SocialLinks";
+// import { ArrowUp } from "lucide-react";
+
+
+// export default function ClientLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   const pathname = usePathname();
+//     const isRoot = pathname === "/";
+//     function handleClick() {
+//         document.getElementById("header")?.scrollIntoView({ behavior: "smooth" });
+//     }
+
+//   return (
+//     <>
+//       {!isRoot && (
+//         <>
+//           <Header />
+//                   <SocialLinks />
+                  
+//         </>
+//       )}
+//           {children}
+          
+//           {!isRoot && (
+//               <div className="flex justify-end items-end mr-5 mb-5">
+//               <button onClick={handleClick} className="bg-black/40 border hover:cursor-pointer rounded-lg text-white/70  border-black/70 px-4 flex gap-2 py-2">Back to Top <ArrowUp/></button>
+//           </div>)}
+//     </>
+//   );
+// }
+
 "use client";
 
 import { usePathname } from "next/navigation";
 import Header from "./components/Header";
 import SocialLinks from "./components/SocialLinks";
 import { ArrowUp } from "lucide-react";
+import BackToTop from "./components/BackToTop";
 
 
 export default function ClientLayout({
@@ -19,19 +58,18 @@ export default function ClientLayout({
 
   return (
     <>
-      {!isRoot && (
+      
         <>
           <Header />
                   <SocialLinks />
                   
         </>
-      )}
+      
           {children}
           
-          {!isRoot && (
-              <div className="flex justify-end items-end mr-5 mb-5">
-              <button onClick={handleClick} className="bg-black/40 border hover:cursor-pointer rounded-lg text-white/70  border-black/70 px-4 flex gap-2 py-2">Back to Top <ArrowUp/></button>
-          </div>)}
+          
+              <BackToTop></BackToTop>
+          
     </>
   );
 }
